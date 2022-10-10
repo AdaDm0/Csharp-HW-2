1d36bf2be3fc171ad51b91baf1799310bcb02e0e
salikova-sun-94@mail.ru <salikova-sun-94@mail.ru>
2022-10-11 00:10:05 +0300

    -

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -13,29 +13,7 @@ namespace Task_5
             /*Задание 5. Пользователь с клавиатуры вводит в строку арифметическое выражение. Приложение должно посчитать его результат. 
              *Необходимо поддерживать только две операции: + и –.*/
 
-            //Console.WriteLine("Введите арифметическое выражение");
-            //string str = Console.ReadLine();
-
-            string str= "2 + 3";
-
-            if (str.Contains(" "))
-            {
-                str = str.Replace(" ", "");
-            }
-            char[] arr = str.ToCharArray();
-            char a;
-            int b;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (char.IsDigit(arr[i]) && (arr[i]=="+" | arr[i]=="-")) 
-                {
-                    b = i;
-                }
-            }
-
-
-
-            //Console.WriteLine(str);
+            
         }
     }
 }
